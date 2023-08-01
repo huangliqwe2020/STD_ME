657983772cdf199f6dbd98eca341bb11a2d231cb
Yucheng Huang <59169297+huangliqwe2020@users.noreply.github.com>
2023-08-01 15:16:11 +0800

    Update STDefACMix.py

--- STDefACMix.py
+++ STDefACMix.py
@@ -41,22 +41,13 @@ def zeros(tensor):
         tensor.data.fill_(0.0)
 
 class STDefACMixAttn(nn.Module):
-    r"""
-        dim (int): Number of input channels.
-        num_heads (int): Number of attention heads.
-        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
-        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set
-        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
-        proj_drop (float, optional): Dropout ratio of output. Default: 0.0
-    """
+   
 
     def __init__(self, dim, num_heads, H, W, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.):
 
         super().__init__()
         self.dim = dim
         self.num_heads = num_heads
-        # head_dim = dim // num_heads
-        # self.scale = qk_scale or head_dim ** -0.5
         self.qkv = nn.Linear(dim, dim//4, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop)
         self.proj_drop = nn.Dropout(proj_drop)
@@ -79,7 +70,6 @@ class STDefACMixAttn(nn.Module):
         self.H = H
         self.W = W
 
-        # fully connected layer in Fig.2
         self.fc = nn.Conv2d(self.num_heads*2, 9, kernel_size=1, bias=True).cuda()
         self.large = nn.Conv2d(64, 512, kernel_size=1, bias=True).cuda()
 
